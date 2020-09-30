b5403743ea95dab7800f0390ad68a362642c5661
dethaa <sharonmarbun12@gmail.com>
2020-09-30 21:38:20 +0700

    metode reduksi baris done

--- Determinan.java
+++ Determinan.java
@@ -5,20 +5,48 @@ import java.io.*;
 public class Determinan extends MATRIKS {
     Scanner scan = new Scanner(System.in);
     
-    public static void detreduksi(){
+    public static void detReduksi(){
         MATRIKS m = new MATRIKS();
         double det=0;
         m.bacaMatriks1();
         if (m.isSquare()){
             if (m.getBrs()==2){
                 det=((m.getElmt(0, 0)*m.getElmt(1, 1)) - (m.getElmt(1, 0)*m.getElmt(0, 1)));
-            } 
+            } else {
+                int nSwap=1;
+                double leadKoef;
+                for(int i=0;i<m.getBrs();i++){
+                    if(!m.isBrsZero(i)){
+                        int k=i+1;
+                        while ((m.getLead(i)!=i) && k<m.getBrs()){
+                            m.swap(i, k);
+                            nSwap*=(-1);
+                            k++;
+                        } 
+                        if(!(k == m.getBrs())){
+                            leadKoef = m.matriks[i][m.getLead(i)];
+                            for (int j=i+1; j<m.getBrs();j++){
+                                if(!m.isBrsZero(j)){
+                                    if (m.getElmt(j, i)!=0){
+                                        m.minus(j, i, 1, (m.getElmt(j, i)/leadKoef));
+                                    }
+                                }
+                            }
+                        }
+                    } 
+                }
+                det++;
+                for (int a=0;a<m.getBrs();a++){
+                    det*=m.getElmt(a, a);
+                }
+                det*=nSwap;
+            }
             System.out.println("Determinannya adalah " + det);
         } else {
             System.out.println("Bukan merupakan matriks persegi, determinan tidak dapat dicari.");
         } 
     }
-    public static double detkofaktor(MATRIKS m){
+    public static double detKofaktor(MATRIKS m){
         MATRIKS temp = new MATRIKS();
         double det = 0;
         if (m.getBrs()==2){
@@ -26,9 +54,9 @@ public class Determinan extends MATRIKS {
         }else {
             int i,j,k;
             temp.makeMATRIKS(m.getBrs(), m.getKol());
-            for(k=0;k<=m.getKol()-1;k++){
-                for (i=1;i<=m.getBrs()-1;i++){
-                    for (j=0;j<=m.getKol()-1;j++){
+            for(k=0;k<m.getKol();k++){
+                for (i=1;i<m.getBrs();i++){
+                    for (j=0;j<m.getKol();j++){
                         if (j>k){
                             temp.matriks[i-1][j-1] = m.getElmt(i, j);
                         } else{

29df1ca3127bbbf6acfb6c96bfe352ef3f00fb7b
roysimbolon <13519068@std.stei.itb.ac.id>
2020-09-30 20:53:02 +0700

    tmbah makeIdentitas,gabungMatriks,potongMatriks

--- MATRIKS.java
+++ MATRIKS.java
@@ -1,4 +1,9 @@
-package tubes1algeo;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes_1algeo;
 
 
 import java.util.Scanner;
@@ -186,4 +191,105 @@ public class MATRIKS {
             }
         }
     }
-}
+    
+    public void makeIdentitas(){
+         MATRIKS m = new MATRIKS();
+        m.bacaMatriks1();
+        
+        
+       
+        
+     
+       
+        if (m.kolom==m.baris){
+            int i=0;
+            while(i<= m.baris){
+                int j=0;
+                while (j<=m.kolom){
+                    if(i!=j){
+                        this.matriks[i][j]=0;
+                      
+                    }
+                    else{
+                        this.matriks[i][j]=1;
+                        
+                    }
+                    j=j+1;
+                }
+                i=i+1;
+            
+            }
+            
+        }
+    }
+    
+    
+     public MATRIKS gabungMatriks(MATRIKS N1, MATRIKS N2){
+        
+        MATRIKS gabungan = new MATRIKS();
+        gabungan.MATRIKS(N1.baris,(N1.kolom+N2.kolom));
+
+
+
+        if (N1.baris == N2.baris){
+            int i=0;
+            while(i<= N1.baris){
+                int j=0;
+                while (j<=N2.kolom){
+            
+                    gabungan.matriks[i][j]=N1.matriks[i][j];
+                    
+                    j=j+1;
+                }
+                i=i+1;
+            
+            }
+            i=0;
+            while(i<= N1.baris){
+                int j=0;
+                while (j<=N2.kolom){
+                    gabungan.matriks[i][j] = N2.matriks[i][j-N1.kolom];
+                    j=j+1;
+                }
+                i=i+1;
+            
+            }
+       
+        }
+        else{
+            
+        }
+        return gabungan;
+
+        
+    }
+    
+    public MATRIKS potongMatriks(int a, int b){
+        MATRIKS potong = new MATRIKS();
+        potong.MATRIKS(this.baris ,b-a+1);
+        
+        if ((a >= 0 && a < this.kolom) && (b >= 0 && b < this.kolom) && (a <= b)){
+            
+            for (int i = 0; i < this.baris; i++){
+                int Kolom = 0;
+                for (int j = a; j <= b; j++){
+                    potong.matriks[i][Kolom] = this.matriks[i][j];
+                    Kolom++;
+                }
+            }
+           
+	}
+        else{
+           
+	}
+        return potong;
+    }
+    
+}  
+
+    
+    
+
+
+
+    

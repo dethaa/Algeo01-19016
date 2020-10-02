6e728a89764653610bf228a8b43ee46a9d058f08
roysimbolon <13519068@std.stei.itb.ac.id>
2020-10-02 14:01:15 +0700

    Matriks balikan udah berjalan lancar

--- Matriks_Balikan.java
+++ Matriks_Balikan.java
@@ -1,80 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tubes_1algeo;
 
-/**
- *
- * @author asus
- */
 
 import java.util.Scanner;
 import java.io.*;
 
-
 public class Matriks_Balikan extends MATRIKS {
 
   
-    
-    
-    public static  MATRIKS MatriksBalikanGaussJordan(MATRIKS m){
-      
-    
-        
-
-        
-        //Membuat matriks identitas
-        MATRIKS i = new MATRIKS ();
-        i.MATRIKS(m.baris, m.kolom);
-        MATRIKS j = new MATRIKS ();
-        MATRIKS N = new MATRIKS(); 
-        i.makeIdentitas(i.baris);
-     
-    
-        //matriks inputan digabung matriks identitas
-        N = N.gabungMatriks(m,i);
- 
-        
-        //
+    public static  MATRIKS MatriksBalikanGaussJordan(MATRIKS N){
+        //Melakukan gaussJordan
         N.gaussJordan();
-     
-        
+        //Pemotongan matriks menjadi 2, sebelah kanan matriks balikkannya
+        //  A.I=I.A^(-1)
         MATRIKS r =new MATRIKS ();
-        r.MATRIKS(m.baris, m.kolom);
+        r.MATRIKS(N.baris, N.baris);
         r=N.potongMatriks(N);
        
         return r;
-            
-        
     }
     
     
-    
-   
     public   MATRIKS MatriksBalikanAdjoin(MATRIKS m){
       
-       
- 
         MATRIKS k  = new MATRIKS();
         MATRIKS l = new MATRIKS();
         MATRIKS o = new MATRIKS();
-       
         
-     
-      
         k=m.Mkofaktor(m);
-    
         l=k.transpose(k);
     
         double r = Determinan.detKofaktor(m); 
         double s = 1/r;
        
         o=l.Kali(l,s);
-        
         return o;
-     
-   }
-   
+    }
 }
